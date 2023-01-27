aa2dc04e61d5bba0b8ab22c984f5fcad02bd70a0
Nirmal Kumar <84436580+Nirmal-Kumar97@users.noreply.github.com>
2023-01-27 17:52:21 +0530

    feat: added some files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signUp')
-  signUp(@Req() req: Request) {
-    console.log(req.body);
-    return this.authService.signUp(req.body);
+  signUp(
+    @Body() dto: AuthDto,
+    // @Body('email') email: string,
+    // @Body('passcode') passcode: string,
+    // @Body('passcode') passcode: ParseIntPipe, //To change the passcode string to int
+  ) {
+    return this.authService.signUp(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signIn')
-  signIn() {
-    return this.authService.signIn;
+  signIn(@Body() dto: AuthDto) {
+    return this.authService.signIn(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import {
+  PrismaClientExtensionError,
+  PrismaClientKnownRequestError,
+} from '@prisma/client/runtime';
+import { error } from 'console';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config/dist/config.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  async signUp(req) {
-    const user = await this.prisma.user.create({
-      data: {
-        email: req.email,
-        hash: req.password,
-      },
-    });
-    return user;
+  async signUp(dto: AuthDto) {
+    const passcode = await argon.hash(dto.passcode);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          passcode,
+        },
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
+
+      // delete user.passcode;
+      // return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+    }
+    throw error;
   }
 
-  signIn() {
-    return { msg: 'I am signed in' };
+  async signIn(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials Incorrect');
+    const pwMatches = await argon.verify(user.passcode, dto.passcode);
+    if (!pwMatches) throw new ForbiddenException('Credentials Incorrect');
+
+    return this.signtoken(user.id, user.email);
+  }
+
+  async signtoken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payLoad = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payLoad, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
